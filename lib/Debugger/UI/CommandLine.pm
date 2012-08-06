3f8d372374922588f3e02d2445f69f97cd7fd9b3
jnthn <jnthn@jnthn.net>
2012-08-07 00:28:54 +0200

    Emit notes about loading new files in the command line UI.

--- lib/Debugger/UI/CommandLine.pm
+++ lib/Debugger/UI/CommandLine.pm
@@ -1,5 +1,13 @@
 module Debug::UI::CommandLine;
 
+# The source code of the files we've encountred while debugging.
+my %sources;
+
+$*DEBUG_HOOKS.set_hook('new_file', -> $filename, $source {
+    say ">>> LOADING $filename";
+    %sources{$filename} = $source;
+});
+
 $*DEBUG_HOOKS.set_hook('statement', -> $text {
     say $text;
     prompt("> ");
