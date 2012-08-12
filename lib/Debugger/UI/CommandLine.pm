90858e41597bb67234cabee9280b0edcb7238e87
jnthn <jnthn@jnthn.net>
2012-08-13 00:16:40 +0200

    Implement handling of uncaught exceptions, showing the line of code that caused the exception and the text of the exception itself. Allows evaluation of code in the context where the exception was thrown.

--- lib/Debugger/UI/CommandLine.pm
+++ lib/Debugger/UI/CommandLine.pm
@@ -36,9 +36,15 @@ my class SourceFile {
         return ($def_line, $def_pos)
     }
     
-    sub normal_lines(@lines) {
+    sub normal_lines(@lines, $color) {
         @lines.map: {
-            colored('| ', 'blue') ~ $_.subst("\r", "")
+            colored('| ', $color) ~ $_.subst("\r", "")
+        }
+    }
+    
+    sub error_lines(@lines) {
+        @lines.map: {
+            colored('| ' ~ $_.subst("\r", ""), 'red')
         }
     }
     
@@ -82,13 +88,30 @@ my class SourceFile {
         $ctx_end = +@!lines - 1 if $ctx_end >= @!lines;
         return join "\n",
             colored("+ $!filename ($ctx_start - $ctx_end)", 'blue'),
-            normal_lines(@!lines[$ctx_start..^$from_line]),
+            normal_lines(@!lines[$ctx_start..^$from_line], 'blue'),
             highlighted_lines(@!lines[$from_line..$to_line], $from_pos, $to_pos),
-            normal_lines(@!lines[$to_line^..$ctx_end]);
+            normal_lines(@!lines[$to_line^..$ctx_end], 'blue');
+    }
+    
+    method exception_summary($e, $line) {
+        my $ctx_start = $line - 2;
+        $ctx_start = 0 if $ctx_start < 0;
+        my $ctx_end = $line + 2;
+        $ctx_end = +@!lines - 1 if $ctx_end >= @!lines;
+        return join "\n",
+            colored("+ Uncaught Exception", 'red'),
+            colored('| ', 'red') ~ $e.message,
+            colored("+ $!filename ($ctx_start - $ctx_end)", 'red'),
+            normal_lines(@!lines[$ctx_start..^$line], 'red'),
+            error_lines([@!lines[$line]]),
+            normal_lines(@!lines[$line^..$ctx_end], 'red');
     }
 }
 
+# Holds the current state of the debugger.
 my class DebugState {
+    my $dying = False;
+    
     method eval_in_ctx($ctx, $code) {
         ENTER $*DEBUG_HOOKS.suspend();
         LEAVE $*DEBUG_HOOKS.unsuspend();
@@ -99,11 +122,23 @@ my class DebugState {
         $pbc();
     }
     
+    method enter_death_throes() {
+        # Or is that the death throws?
+        $dying = True;
+    }
+    
     method issue_prompt($ctx) {
         loop {
-            given prompt(colored('> ', 'blue')) {
+            given prompt(colored('> ', $dying ?? 'red' !! 'blue')) {
                 when '' {
-                    return;
+                    if $dying {
+                        say colored(
+                            'Cannot continue execution after an unhandled exception',
+                            'red');
+                    }
+                    else {
+                        return;
+                    }
                 }
                 when /^ < p print s say > \s+ (.+)/ {
                     say self.eval_in_ctx($ctx, ~$0);
@@ -166,3 +201,34 @@ $*DEBUG_HOOKS.set_hook('statement_cond', -> $filename, $ctx, $type, $from, $to {
     say %sources{$filename}.summary_around($from, $to);
     DebugState.issue_prompt($ctx);
 });
+
+# Override handler for uncaught exceptions.
+my Mu $p6comp := pir::compreg__Ps('perl6');
+$p6comp.HOW.find_method($p6comp, 'handle-exception').wrap(-> |$ {
+    my Mu $vm_ex := nqp::atpos(pir::perl6_current_args_rpa__P(), 1);
+    pir::perl6_invoke_catchhandler__vPP(&unhandled, $vm_ex);
+});
+sub unhandled(|$) {
+    my Mu $vm_ex := nqp::atpos(pir::perl6_current_args_rpa__P(), 0);
+    my $e = EXCEPTION($vm_ex);
+    my $bt = $e.backtrace();
+    my $ctx = CALLER;
+    my ($file, $line);
+    for @$bt {
+        if %sources.exists(.file) {
+            $file = .file;
+            $line = .line;
+            last;
+        }
+        $ctx = $ctx.WHO.<CALLER>;
+    }
+    if $file {
+        DebugState.enter_death_throes();
+        say %sources{$file}.exception_summary($e, $line - 1);
+        DebugState.issue_prompt($ctx.WHO);
+    }
+    else {
+        say "Unhandled exception: $e.message() @ $file:$line";
+        exit(0);
+    }
+}
