0f4dcf15eba1a28d1346dee90abc3986504ff070
Tadeusz Sośnierz <tadzikes@gmail.com>
2012-12-06 23:27:29 +0100

    Make it Panda-installable

---
+++ Build.pm
@@ -0,0 +1,18 @@
+use Panda::Common;
+use Panda::Builder;
+
+my $nqp        = 'nqp';
+my $parrot     = 'parrot';
+my $pbc_to_exe = 'pbc_to_exe';
+my $executable = $*OS eq 'MSWin32' ?? 'perl6-debug.exe' !! 'perl6-debug';
+
+class Build is Panda::Builder {
+    method build(Pies::Project $p) {
+        my $workdir = $.resources.workdir($p);
+
+        shell "$nqp --vmlibs=perl6_group,perl6_ops --target=pir "
+            ~ "--output=perl6-debug.pir bin/perl6-debug.nqp";
+        shell "$parrot -o perl6-debug.pbc perl6-debug.pir";
+        shell "$pbc_to_exe --output=bin/$executable perl6-debug.pbc"
+    }
+}
