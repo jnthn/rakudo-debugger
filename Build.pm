3aff7eb14a841b0cbdd164c290c921b973941af1
Timo Paulssen <timonator@perpetuum-immobile.de>
2013-05-24 15:03:24 +0200

    make Build.pm correspond with changes in current Panda.

--- Build.pm
+++ Build.pm
@@ -7,9 +7,7 @@ my $pbc_to_exe = 'pbc_to_exe';
 my $executable = $*OS eq 'MSWin32' ?? 'perl6-debug.exe' !! 'perl6-debug';
 
 class Build is Panda::Builder {
-    method build(Pies::Project $p) {
-        my $workdir = $.resources.workdir($p);
-
+    method build(Str $path) {
         shell "$nqp --vmlibs=perl6_group,perl6_ops --target=pir "
             ~ "--output=perl6-debug.pir bin/perl6-debug.nqp";
         shell "$parrot -o perl6-debug.pbc perl6-debug.pir";
