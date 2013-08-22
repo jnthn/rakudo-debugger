6fd1d22cbf07ac77203077403be88187a4b64f9b
jnthn <jnthn@jnthn.net>
2013-08-22 22:55:06 +0200

    Remove mention of perl6_group, which went away.

--- Build.pm
+++ Build.pm
@@ -8,7 +8,7 @@ my $executable = $*OS eq 'MSWin32' ?? 'perl6-debug.exe' !! 'perl6-debug';
 
 class Build is Panda::Builder {
     method build(Str $path) {
-        shell "$nqp --vmlibs=perl6_group,perl6_ops --target=pir "
+        shell "$nqp --vmlibs=perl6_ops --target=pir "
             ~ "--output=perl6-debug.pir bin/perl6-debug.nqp";
         shell "$parrot -o perl6-debug.pbc perl6-debug.pir";
         shell "$pbc_to_exe --output=bin/$executable perl6-debug.pbc"
